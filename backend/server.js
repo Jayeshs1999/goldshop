e9b15d396478fdf84fea39701aed2550376cd5d5
Jayeshs1999 <jayeshsevatkar55@gmail.com>
2024-04-03 00:46:24 +0530

    push Ctrl+Shift+G fronted

--- backend/server.js
+++ backend/server.js
@@ -14,6 +14,19 @@ app.get("/", (req, res) => {
 });
 
 app.use("/api/users", userRoute);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "/frontend/build")));
+
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"))
+  );
+} else {
+  app.get("/", (req, res) => {
+    res.send("API is running");
+  });
+}
+
 app.use(notFound);
 app.use(errorHandler);
 
