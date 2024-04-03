96c1a1d5843108690dd75a69fd880af0a3ad71a9
Jayeshs1999 <jayeshsevatkar55@gmail.com>
2024-04-03 11:02:31 +0530

    added images'

--- frontend/src/screens/Landing.tsx
+++ frontend/src/screens/Landing.tsx
@@ -1,61 +1,51 @@
 import React from "react";
 import Card from "../utils/Card";
-import goldImage from "../assets/px.jpg";
+import { img1, img2 } from "../assets";
 
 const Landing = () => {
   const goldArray = [
     {
-      imageSrc: goldImage,
-      heading: "Beautiful Card",
-      subtitle: "This is a beautiful card with a fixed image.",
+      imageSrc: img1,
+      heading: "Ring",
+      subtitle: "Starting from 760000",
     },
     {
-      imageSrc: goldImage,
-      heading: "Beautiful Card",
-      subtitle: "This is a beautiful card with a fixed image.",
+      imageSrc: img2,
+      heading: "Gold Phone",
+      subtitle: "Starting from 760000",
     },
     {
-      imageSrc: goldImage,
-      heading: "Beautiful Card",
-      subtitle: "This is a beautiful card with a fixed image.",
+      imageSrc: img1,
+      heading: "Gold Laptop",
+      subtitle: "Starting from 760000",
     },
     {
-      imageSrc: goldImage,
-      heading: "Beautiful Card",
-      subtitle: "This is a beautiful card with a fixed image.",
+      imageSrc: img2,
+      heading: "Watch",
+      subtitle: "Starting from 30000",
     },
     {
-      imageSrc: goldImage,
-      heading: "Beautiful Card",
-      subtitle: "This is a beautiful card with a fixed image.",
+      imageSrc: img1,
+      heading: "Table",
+      subtitle: "Starting from 40000",
     },
     {
-      imageSrc: goldImage,
-      heading: "Beautiful Card",
-      subtitle: "This is a beautiful card with a fixed image.",
+      imageSrc: img2,
+      heading: "Gold Laptop",
+      subtitle: "Starting from 760000",
     },
     {
-      imageSrc: goldImage,
-      heading: "Beautiful Card",
-      subtitle: "This is a beautiful card with a fixed image.",
+      imageSrc: img1,
+      heading: "Watch",
+      subtitle: "Starting from 30000",
     },
     {
-      imageSrc: goldImage,
-      heading: "Beautiful Card",
-      subtitle: "This is a beautiful card with a fixed image.",
+      imageSrc: img2,
+      heading: "Table",
+      subtitle: "Starting from 40000",
     },
     {
-      imageSrc: goldImage,
-      heading: "Beautiful Card",
-      subtitle: "This is a beautiful card with a fixed image.",
-    },
-    {
-      imageSrc: goldImage,
-      heading: "Beautiful Card",
-      subtitle: "This is a beautiful card with a fixed image.",
-    },
-    {
-      imageSrc: goldImage,
+      imageSrc: img1,
       heading: "Beautiful Card",
       subtitle: "This is a beautiful card with a fixed image.",
     },
